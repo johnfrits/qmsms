13f5b6b56047aaeb03e414621190a327924545de
Johnfrits Rejaba <frats.frets.frits@gmail.com>
2017-07-11 01:43:08 +0800

    some changes
    some changes

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap 101 Template</title>
-    <link href="Scripts/css/bootstrap.min.css" rel="stylesheet">
-    <link href="Scripts/css/bootstrap.css" rel="stylesheet">
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
   </head>
   <body>
     <h1>Hello, world!</h1>
@@ -90,6 +90,6 @@
 </ul>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="Scripts/js/bootstrap.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
   </body>
 </html>
